c42ce31fbcb8aeda38755715f7238244c2b66b0d
cccaternberg <acaternberg@cloudbees.com>
2023-12-15 16:30:17 +0100

    add info

--- createServiceAccountForNamespace.sh
+++ createServiceAccountForNamespace.sh
@@ -2,10 +2,12 @@
 export SERVICEACCOUNT_NAME="myserviceaccount"
 export NAMESPACE="test"
 
+echo "####################################################################################################"
 echo "create namespace $NAMESPACE. We want to create a Serviceaccount in this namespace"
 kubectl create ns $NAMESPACE
 kubectl config set-context --current --namespace=$NAMESPACE
 
+echo "####################################################################################################"
 echo "Apply ServiceAccount $SERVICEACCOUNT_NAME, Role, RoleBinding and a ServiceAccount secret"
 kubectl apply -f - <<EOF
 apiVersion: v1
@@ -47,23 +49,26 @@ metadata:
 type: kubernetes.io/service-account-token
 EOF
 
+echo "####################################################################################################"
 echo "Show the secret/$SERVICEACCOUNT_NAME-token so we can see that Kubernetes has added a valid cert and token"
 kubectl get secret/$SERVICEACCOUNT_NAME-token  -o yaml
 
+echo "####################################################################################################"
 echo "export variables that we will need for creating the kubeconfig file"
 export USER_TOKEN_VALUE=$(kubectl get secret/$SERVICEACCOUNT_NAME-token -o=go-template='{{.data.token}}' | base64 --decode)
 export CURRENT_CONTEXT=$(kubectl config current-context)
 export CURRENT_CLUSTER=$(kubectl config view --raw -o=go-template='{{range .contexts}}{{if eq .name "'''${CURRENT_CONTEXT}'''"}}{{ index .context "cluster" }}{{end}}{{end}}')
 export CLUSTER_CA=$(kubectl config view --raw -o=go-template='{{range .clusters}}{{if eq .name "'''${CURRENT_CLUSTER}'''"}}"{{with index .cluster "certificate-authority-data" }}{{.}}{{end}}"{{ end }}{{ end }}')
 export CLUSTER_SERVER=$(kubectl config view --raw -o=go-template='{{range .clusters}}{{if eq .name "'''${CURRENT_CLUSTER}'''"}}{{ .cluster.server }}{{end}}{{ end }}')
-#echo "USER_TOKEN_VALUE: $USER_TOKEN_VALUE"
-#echo "CURRENT_CONTEXT: $CURRENT_CONTEXT"
-#echo "CURRENT_CLUSTER: $CURRENT_CLUSTER"
-#echo "CLUSTER_CA: $CLUSTER_CA"
-#echo "CLUSTER_SERVER: $CLUSTER_SERVER"
+echo "USER_TOKEN_VALUE: $USER_TOKEN_VALUE"
+echo "CURRENT_CONTEXT: $CURRENT_CONTEXT"
+echo "CURRENT_CLUSTER: $CURRENT_CLUSTER"
+echo "CLUSTER_CA: $CLUSTER_CA"
+echo "CLUSTER_SERVER: $CLUSTER_SERVER"
 
-export -p USER_TOKEN_VALUE CURRENT_CONTEXT CURRENT_CLUSTER CLUSTER_CA CLUSTER_SERVER
+#export -p USER_TOKEN_VALUE CURRENT_CONTEXT CURRENT_CLUSTER CLUSTER_CA CLUSTER_SERVER
 
+echo "####################################################################################################"
 echo "Create local kubeconfig file for Serviceaccount $SERVICEACCOUNT_NAME"
 cat << EOF > kubeconfig-${SERVICEACCOUNT_NAME}
 apiVersion: v1
@@ -85,7 +90,9 @@ users:
   user:
     token: ${USER_TOKEN_VALUE}
 EOF
+cat kubeconfig-${SERVICEACCOUNT_NAME}
 
+echo "####################################################################################################"
 echo "Apply a test pod using the Serviceaccount  $SERVICEACCOUNT_NAME"
 POD_NAME=nginxpod
 kubectl --kubeconfig $(pwd)/kubeconfig-${SERVICEACCOUNT_NAME} apply -f - <<EOF
@@ -100,7 +107,7 @@ spec:
   serviceAccountName: $SERVICEACCOUNT_NAME
 EOF
 
-
+echo "####################################################################################################"
 echo "delete pod"
 kubectl --kubeconfig $(pwd)/kubeconfig-${SERVICEACCOUNT_NAME}  delete pod $POD_NAME
 echo "delete Namespace"
